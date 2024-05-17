291e7e3ee2900557dafe952e3095a1fa79bf8c85
AnuragPhukan <anuragphukan5@gmail.com>
2024-05-17 13:35:04 +0100

    Add files via upload

---
+++ data/authorized_tags.js
@@ -0,0 +1,16 @@
+define({
+    authorizedTags: {
+        "type:system": {
+            "description": "paper describing a system"
+        },
+        "type:survey": {
+            "description": "survey paper"
+        },
+        "type:analysis": {
+            "description": "analytical study"
+        },
+        "type:experiment": {
+            "description": "experimental study"
+        }
+    }
+});
---
+++ data/tag_categories.js
@@ -0,0 +1,10 @@
+define({
+    tagCategories: {
+        "research_category": {
+            "description": "Classification of research papers by topic"
+        },
+        "evaluation_method": {
+            "description": "Evaluation methods used in the research papers"
+        }
+    }
+});
